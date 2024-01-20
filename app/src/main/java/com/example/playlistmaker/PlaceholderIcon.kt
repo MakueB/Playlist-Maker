fe9da9ac51e954242bf869199a175ff5e5c56e49
MakueB <makbones@gmail.com>
2024-01-19 23:45:30 -0500

    11.0

---
+++ app/src/main/java/com/example/playlistmaker/PlaceholderIcon.kt
@@ -0,0 +1,6 @@
+package com.example.playlistmaker
+
+enum class PlaceholderIcon(val resourceId: Int) {
+    nothingFound(R.drawable.nothing_found_light),
+    noInternet(R.drawable.no_internet_light)
+}
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,58 +1,154 @@
 package com.example.playlistmaker
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.Icon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.text.Editable
-import android.text.Layout
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
-import androidx.core.content.ContextCompat.getSystemService
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
     private var text = TEXT_DEF
     lateinit var editText: EditText
+    lateinit var placeHolderImage: ImageView
+    lateinit var smthWrongMessage: TextView
+    lateinit var refreshButton: Button
+
+    private val iTunesBaseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesService = retrofit.create(ITunesApi::class.java)
+
+    private val trackList = ArrayList<Track>()
+    private val adapter = TrackAdapter()
+
+    private fun search() {
+        val call = iTunesService.search(editText.text.toString()).enqueue(object : Callback<ITunesResponse> {
+            override fun onResponse(
+                call: Call<ITunesResponse>,
+                response: retrofit2.Response<ITunesResponse>
+            ) {
+                if (response.code() == 200){
+                    Log.d("y", response.body()?.results.toString())
+                    if (response.body()?.results?.isNotEmpty() == true){
+                        trackList.clear()
+                        trackList.addAll(response.body()?.results!!)
+                        adapter.notifyDataSetChanged()
+                        showPlaceholder(null)
+                        showMessage("", "")
+                        showButton(false)
+                    } else {
+                        showPlaceholder(PlaceholderIcon.nothingFound)
+                        showMessage("Ничего не нашлось","")
+                        showButton(false)
+                        Log.d("y", response.body()?.results.toString())
+                    }
+                } else {
+                    showPlaceholder(PlaceholderIcon.noInternet)
+                    showMessage("Проблемы со связью", "Загрузка не удалась. Проверьте подключение к интернету")
+                    showButton(true)
+                }
+            }
+
+            override fun onFailure(call: Call<ITunesResponse>, t: Throwable) {
+                showPlaceholder(PlaceholderIcon.noInternet)
+                showMessage("Проблемы со связью", "Загрузка не удалась. Проверьте подключение к интернету")
+                showButton(true)
+            }
+        })
+    }
+
+    private fun showButton(show: Boolean) {
+        if (show) {
+            refreshButton.visibility = View.VISIBLE
+        } else
+            refreshButton.visibility = View.GONE
+    }
+
+    private fun showPlaceholder(icon: PlaceholderIcon?) {
+        if (icon != null) {
+            placeHolderImage.setImageResource(icon.resourceId)
+            placeHolderImage.visibility = View.VISIBLE
+        } else
+            placeHolderImage.visibility = View.GONE
+    }
+
+    private fun showMessage(text: String, additionalText: String) {
+        if (text.isNotEmpty()) {
+            smthWrongMessage.visibility = View.VISIBLE
+            trackList.clear()
+            adapter.notifyDataSetChanged()
+            smthWrongMessage.text = text
+            if (additionalText.isNotEmpty()){
+                smthWrongMessage.text = "$text\n\n$additionalText"
+            }
+        } else
+            smthWrongMessage.visibility = View.GONE
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val trackList = TrackRepository.trackList
-
+        //val trackList = TrackRepository.trackList
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val trackAdapter = TrackAdapter(trackList)
-        recyclerView.adapter = trackAdapter
+        adapter.tracks = trackList
+        recyclerView.adapter = adapter
 
         val linearLayout = findViewById<LinearLayout>(R.id.searchContainer)
         editText = findViewById<EditText>(R.id.editText)
         val clearButton = findViewById<ImageView>(R.id.imageViewClear)
         val backButton = findViewById<ImageView>(R.id.backArrow)
-
-/*        if (savedInstanceState != null){
-            text = savedInstanceState.getString(TEXT, TEXT_DEF)
-            editText.setText(text)
-            Log.d("my", "onRestoreInstanceState: Restored text - $text")
-        }*/
+        placeHolderImage = findViewById<ImageView>(R.id.searchPlaceholder)
+        smthWrongMessage = findViewById(R.id.somethingWrongTexView)
+        refreshButton = findViewById(R.id.refreshButton)
 
         editText.requestFocus()
         showKeyboard(editText)
+
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                search()
+                true
+            }
+            false
+        }
 
         clearButton.setOnClickListener {
             editText.setText("")
         }
 
         backButton.setOnClickListener {
-            super.onBackPressed()
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        refreshButton.setOnClickListener {
+            search()
         }
 
         val textWatcher = object : TextWatcher {
@@ -69,6 +165,7 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         editText.addTextChangedListener(textWatcher)
+
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
