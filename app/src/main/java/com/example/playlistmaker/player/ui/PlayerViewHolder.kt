0206fa8467cfa6b90d2d2bf284257ca2f0cdf991
MakueB <makbones@gmail.com>
2024-11-17 13:43:21 +0300

    22.014

--- app/src/main/java/com/example/playlistmaker/player/ui/PlayerViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/PlayerViewHolder.kt
@@ -1,14 +1,9 @@
 package com.example.playlistmaker.player.ui
 
-import android.graphics.drawable.Drawable
 import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.PlaylistViewBottomSheetBinding
 import com.example.playlistmaker.newplaylist.domain.models.Playlist
@@ -33,30 +28,6 @@ class PlayerViewHolder(private val binding: PlaylistViewBottomSheetBinding) :
                 .placeholder(R.drawable.placeholder)
                 .centerCrop()
                 .transform(RoundedCorners(cornersInPx))
-                .error(R.drawable.placeholder) // Плейсхолдер на случай ошибки
-                .listener(object : RequestListener<Drawable> {
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<Drawable>,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        Log.e("PlayerViewHolder", "Glide Load Failed", e)
-                        return false // Вернуть false для передачи ошибки дальше
-                    }
-
-                    override fun onResourceReady(
-                        resource: Drawable,
-                        model: Any,
-                        target: Target<Drawable>?,
-                        dataSource: DataSource,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        Log.d("PlayerViewHolder", "Glide Load Successful")
-                        return false
-                    }
-                })
                 .into(binding.image)
-
     }
 }
